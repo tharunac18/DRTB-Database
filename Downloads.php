7fb38bed572a43f2a5acac0b523eaa5a0d6c1f74
tharunac18 <167528812+tharunac18@users.noreply.github.com>
2024-04-19 18:12:43 +0530

    Update Downloads.php

--- Downloads.php
+++ Downloads.php
@@ -1,24 +1,106 @@
 <!DOCTYPE html>
 <html>
-<meta charset="UTF-8">
+<head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-        <title>DRTB-DB - Database for Drug Resistant Tuberculosis</title>
-        <h3 style="text-align: center;margin-top: 40px;margin-bottom: 40px;">DRTB-DB - Database for Drug Resistant Tuberculosis</h3>
-    </head>
-    <body>
-    <div style="list-style: none;text-align: center;font-size:20px;">
-        <li style="display: inline-block;padding-right: 20px;"><a href="index.html">Home</a></li>
-        <li style="display: inline-block;padding-right: 20px;"><a href="search.html">Search</a></li>
-        <li style="display: inline-block;padding-right: 20px;"><a  href="submit.html">Submit</a></li>
-        <li style="display: inline-block;padding-right: 20px;"><a  href="Downloads.html">Downloads</a></li>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Roboto', sans-serif;
+        }
+        .nav-item {
+            margin-right: 20px;
+        }
+        .bg-light-grey {
+            background-color: #f4f4f4;
+        }
+        .bg-light-blue {
+            background-color: #eaf9fd;
+        }
+        .bg-light-green {
+            background-color: #f5fde6;
+        }
+        .text-brown {
+            color: #8b572a;
+        }
+        .border-brown {
+            border-color: #8b572a;
+        }
+        .rounded-15 {
+            border-radius: 15px;
+        }
+        .text-center {
+            text-align: center;
+        }
+        .mt-40 {
+            margin-top: 40px;
+        }
+        .mb-40 {
+            margin-bottom: 40px;
+        }
+        .p-20 {
+            padding: 20px;
+        }
+        .fs-25 {
+            font-size: 25px;
+        }
+        .fs-20 {
+            font-size: 20px;
+        }
+        .fs-15 {
+            font-size: 15px;
+        }
+        .fw-bold {
+            font-weight: bold;
+        }
+        .hr-brown {
+            border-top: 2px solid #8b572a;
+        }
+    </style>
+    <title>DRTB-DB - Database for Drug Resistant Tuberculosis</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">DRTB-DB</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="search.html">Search</a>
+                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="submit.html">Submit</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Downloads.html">Downloads</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container mt-40 mb-40 rounded-15 p-20 bg-light-grey">
+        <div class="text-center">
+            <p class="fs-20">Download the database data from the below link</p>
+            <a href="https://github.com/tharunac18/DRTB-Database/blob/main/DRTB.csv" class="btn btn-lg btn-outline-primary mt-20">Download</a>
+        </div>
     </div>
-    <div style="background-color: rgb(224,255,255);margin-top: 40px;margin-bottom: 40px;border-width:2px; border-style:solid; border-color:brown;margin-right: 200px;margin-left: 200px;border-radius: 15px;">
-        <p style="text-align:center;margin-top: 40px;margin-bottom: 40px;font-size:20px;"> Download the database data from the below link <br> <li style="list-style: none;text-align: center;font-size:20px;"> <a href="https://github.com/tharunac18/DRTB-Database/blob/main/DRTB.csv"> <b> Download </a></li> </p>
+    <div class="hr-brown mt-40 mb-40"></div>
+    <div class="text-center mb-40">
+        <p class="fs-15">
+            Department of Biotechnology <br>
+            PSG College of Technology
+        </p>
     </div>
-
-<hr>
-
-<h6 style="text-align:center;margin-top: 40px;margin-bottom: 40px;">Department of Biotechnology <br> PSG College of Technology</h6>        
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
 </html>
